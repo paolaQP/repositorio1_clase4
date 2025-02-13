118137588a63d90fcae39e85d70047bda293827b
Vanessa <Vanessa@DESKTOP-754GRP1>
2025-02-12 20:56:09 -0500

    iarchivo

--- src/programas/Programa1.java
+++ src/programas/Programa1.java
@@ -4,5 +4,6 @@ package programas;
 public class Programa1 {
     public static void main (String args[]) {
         System.out.println("Hola mundo");
+        System.out.println("Bienvenidos al curso de DataOps");
     }
 }
